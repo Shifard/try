e6d33225f0896f262a25c266dc97352181c66a7e
Shifard <118987903+Shifard@users.noreply.github.com>
2025-09-25 10:27:36 +0800

    Add redirection for clickable element

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+const getTUNEYEButton = document.querySelector('.get-TUN-eye');
+
+getTUNEYEButton.addEventListener('click', () => {
+  window.open('https://www.example.com', '_blank');
+});
+
+const sourceCode = document.querySelector('.code');
+sourceCode.addEventListener('click', () => {
+  window.open('https://github.com/veenoise/Tun-Eye-Tool', '_blank');
+});
